4a7f15959f6006f7b9303bd847bffb9ad6b28290
Andrew LaCharite <adlacharite@gmail.com>
2020-04-13 17:51:06 -0400

    add picture and cake

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <birthday msg="Happy Birthday!"/>
+    <img alt="Vue logo" src="./assets/dadonboat.jpg">
+    <cake></cake>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import birthday from './components/birthday.vue'
+import cake from './components/cake.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    birthday,
+    cake
   }
 }
 </script>
---
+++ src/components/cake.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cake">
+    <h2>Snuff out the candles!</h2>
+    <div id="container">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cake',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style >
+#container {
+  width: 300px;
+  height: 300px;
+  text-align: center;
+  background: rgba(36, 22, 9, 0.959);
+}
+</style>
