d6795f18a9e6ba0c692358463b04b9b5f676d2e3
Andrew LaCharite <adlacharite@gmail.com>
2020-04-13 20:53:59 -0400

    add candles and text

--- src/components/cake.vue
+++ src/components/cake.vue
@@ -1,7 +1,78 @@
 <template>
   <div class="cake">
-    <h2>Snuff out the candles!</h2>
-    <div id="container">
+    <h2>Make a wish!</h2>
+    <div id="outer-container">
+      <div id="frosting"></div>
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+      <img src="../assets/candle.png" id="candle">
+    </div>
+    <div id="bottom-text">
+      <h4>Candles are for viewing purposes only. Please do not attempt to extinguish the flames</h4>
+      <h4>Confidentiality Note:  the information transmitted is intended for the person, persons, or entity to whom it is addressed, and may contain confidential, private and/or privileged material.  Any review, retransmission, or other use of this information by persons or entities other than the intended recipient is prohibited.  If you received this in error, please notify the original sender immediately by return e-mail and delete this message and any attachments from your computer.  This message is and effort to protect the confidentiality of this transmission and does not constitute a guarantee.  Thank you.</h4>
     </div>
   </div>
 </template>
@@ -16,10 +87,34 @@ export default {
 </script>
 
 <style >
-#container {
+#outer-container {
   width: 300px;
-  height: 300px;
-  text-align: center;
+  height: 350px;
+  margin-left: auto; 
+  margin-right: auto;
+}
+#frosting {
+  border-radius: 50px;
+  width: 310px;
+  height: 310px;
+  margin-left: auto; 
+  margin-right: auto;
+  position: relative;
+  top: 50px;
+  z-index: -1;
   background: rgba(36, 22, 9, 0.959);
+}
+#candle {
+  position: relative;
+  bottom: 300px;
+  left: 5px;
+}
+#bottom-text {
+  width: 600px;
+  margin-left: auto; 
+  margin-right: auto;
+}
+h4 {
+  text-align: left;
 }
 </style>
